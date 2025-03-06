8087a0b0d601c54a1600dac7fc30bb21e994f9a3
ArminMolnar <molnar.armin9@gmail.com>
2025-03-06 15:50:39 +0100

    create avgResultLabel

--- src/main/java/components/LabelFactory.java
+++ src/main/java/components/LabelFactory.java
@@ -12,11 +12,13 @@ public class LabelFactory {
     private final JLabel diastolicLabel;
     private final JLabel pulseLabel;
     private final JLabel pulsePressureLabel;
+    private final JLabel avgResultLabel;
 
     public LabelFactory() {
         this.nameLabel = createLabel("Name:", 180, 75, 200, 30);
         this.passwordLabel = createLabel("Password:", 180, 75, 200, 30);
-        this.averageLabel = createLabel("Average:", 50, 320, 600, 30);
+        this.averageLabel = createLabel("Average:", 200, 320, 600, 30);
+        this.avgResultLabel = createLabel("", 20, 340, 600, 30);
         this.systolicLabel = createLabel("Systolic:", 50, 75, 200, 30);
         this.diastolicLabel = createLabel("Diastolic:", 160, 75, 200, 30);
         this.pulseLabel = createLabel("Pulse:", 275, 75, 200, 30);
@@ -28,7 +30,7 @@ public class LabelFactory {
         JLabel label = new JLabel(text);
         label.setText(text);
         label.setBounds(x, y, width, height);
-        label.setFont(new Font("Times New Roman", Font.BOLD, 15));
+        label.setFont(new Font("Times New Roman", Font.BOLD, 14));
         return label;
     }
 
@@ -58,5 +60,9 @@ public class LabelFactory {
 
     public JLabel getPasswordLabel() {
         return passwordLabel;
+    }
+
+    public JLabel getAvgResultLabel() {
+        return avgResultLabel;
     }
 }
