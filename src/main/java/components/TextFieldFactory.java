62309c56eda7e706847d7238b97151aedcec5cbd
ArminMolnar <molnar.armin9@gmail.com>
2025-03-11 01:08:48 +0100

    reposition textfields

--- src/main/java/components/TextFieldFactory.java
+++ src/main/java/components/TextFieldFactory.java
@@ -1,6 +1,7 @@
 package components;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextFieldFactory {
 
@@ -13,17 +14,17 @@ public class TextFieldFactory {
     private static TextFieldFactory instance;
 
     public TextFieldFactory() {
-        this.nameTextField = createTextField(170, 100, 100, 30);
+        this.nameTextField = createTextField(135, 100, 100, 30);
         this.systolicField = createTextField(50, 100, 50, 30);
-        this.diastolicField = createTextField(160, 100, 50, 30);
-        this.pulseField = createTextField(270, 100, 50, 30);
-        this.pulsePressureField = createTextField(380, 100, 50, 30);
+        this.diastolicField = createTextField(130, 100, 50, 30);
+        this.pulseField = createTextField(210, 100, 50, 30);
+        this.pulsePressureField = createTextField(290, 100, 50, 30);
     }
 
     private static JTextField createTextField(int x, int y, int width, int height) {
         JTextField textField = new JTextField();
         textField.setBounds(x, y, width, height);
-
+        textField.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         return textField;
     }
 
